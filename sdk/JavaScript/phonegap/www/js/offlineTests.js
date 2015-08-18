098414779c387f6b9376cad18e8c239df0befbdd
shrishrirang <shrishrirang@gmail.com>
2015-08-18 16:58:23 -0700

    Implement UT for offline

--- sdk/JavaScript/phonegap/www/js/offlineTests.js
+++ sdk/JavaScript/phonegap/www/js/offlineTests.js
@@ -2,19 +2,44 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // ----------------------------------------------------------------------------
 
+var testServiceUrl = "http://test.com";
+var testServiceKey = "key";
+var testTableName = "items";
+
+function createMobileServiceClient() {
+    return new WindowsAzure.MobileServiceClient(testServiceUrl, testServiceKey);
+}
+
 function getOfflineTests() {
     var tests = [];
 
     tests.push(new zumo.Test('Offline test 1', function (test, done) {
         console.log("Running " + test.name);
 
-        done(true);
-    }));
+        var client = createMobileServiceClient();
+        client.syncContext.initialize();
 
-    tests.push(new zumo.Test('Offline test 2', function (test, done) {
-        console.log("Running " + test.name);
+        var syncTable = client.getSyncTable(testTableName);
 
-        done(false);
+        var rowObject = {
+            "id": "pen",
+            "description": "reynolds",
+            "price": 51
+        };
+
+        return syncTable.insert(rowObject).then(function () {
+            return syncTable.lookup(rowObject.id);
+        }, function () {
+            $assert.fail("Expected insert to be successful");
+        })
+        .then(function (result) {
+            //$assert.isTrue(JSON.stringify(rowObject) === JSON.stringify(result), "MobileServiceSyncTable lookup failed");
+            done(JSON.stringify(rowObject) === JSON.stringify(result));
+            }, function () {
+            //$assert.fail("Lookup should have succeeded");
+                done(false);
+            });
+
     }));
 
     return tests;
